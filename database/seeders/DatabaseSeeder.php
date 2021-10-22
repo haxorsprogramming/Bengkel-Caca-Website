692cd342671bd28cd072465bed4da9d642e9644a
Haxorsprogramming <haxors@uinsu.ac.id>
2021-10-22 14:30:31 +0700

    first commit

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('tbl_user') -> insert([
+            'username' => 'admin',
+            'tipe' => 'super-admin',
+            'password' => password_hash('admin', PASSWORD_DEFAULT),
+            'active' => '1'
+        ]);
+    }
+}
