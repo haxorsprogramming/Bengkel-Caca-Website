f2c3e755f2fdfb5daf38cf3a91356e59461ed1f0
Haxorsprogramming <haxors@uinsu.ac.id>
2021-10-23 10:09:02 +0700

    'update'

--- database/migrations/2021_10_22_015318_tbl_profile_member.php
+++ database/migrations/2021_10_22_015318_tbl_profile_member.php
@@ -13,7 +13,14 @@ class TblProfileMember extends Migration
      */
     public function up()
     {
-        //
+        Schema::create('tbl_profile_member', function (Blueprint $table) {
+            $table -> id();
+            $table -> char('username', 50);
+            $table -> char('nama_lengkap', 20);
+            $table -> date('tanggal_lahir');
+            $table -> char('password', 100); 
+            $table -> char('active', 1);
+        });
     }
 
     /**
@@ -23,6 +30,6 @@ class TblProfileMember extends Migration
      */
     public function down()
     {
-        //
+        Schema::dropIfExists('tbl_profile_member');
     }
 }
