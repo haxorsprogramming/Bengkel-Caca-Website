2036f2e17f7c794489f79cf80ad4f03feeef6f5e
Haxorsprogramming <haxors@uinsu.ac.id>
2021-10-31 13:29:48 +0700

    'update'

--- public/ladun/base/js/member.js
+++ public/ladun/base/js/member.js
@@ -1,2 +1,16 @@
+// vue object 
+var app_member = new Vue({
+    el : '#app_member',
+    data : {
+
+    },
+    methods : {
+        tambah_member_atc : function()
+        {
+            console.log("haloo");
+        }
+    }
+});
+
 // function 
 $('#tbl_member').dataTable();
--- resources/views/member/member_page.blade.php
+++ resources/views/member/member_page.blade.php
@@ -1,9 +1,9 @@
 <div class="row">
-    <div class="col s12 m12 l12" id="app_tahun_ajaran">
+    <div class="col s12 m12 l12" id="app_member">
         <div class="card" id="d_data_tahun_ajaran">
             <div class="card-content">
                 <span class="card-title">Daftar member bengkel</span>
-                <a href="#!" class="btn">Tambah member baru</a>
+                <a href="#!" class="btn" @click="tambah_member_atc()">Tambah member baru</a>
                 <table id="tbl_member" class="display responsive-table datatable-example">
                     <thead>
                         <tr>
